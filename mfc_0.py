d3c93389acc7a55534c6d3dc2bbf917de3ac6095
gr4ison <polednagraison@gmail.com>
2025-03-02 17:03:36 -0800

    Updated PID

--- mfc_0.py
+++ mfc_0.py
@@ -1,5 +1,12 @@
 import asyncio
 from alicat import FlowController
+import serial
+
+#ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)  # Adjust baud rate if needed
+#ser.write(b'flow?\r')  # Example command for Alicat
+#response = ser.readline()
+#print(response.decode('utf-8'))
+#ser.close()
 
 async def get():
     async with FlowController('/dev/ttyUSB0') as flow_controller:
